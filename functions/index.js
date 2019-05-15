8f6fdcec2097ad6bf8634400ac67854359269768
Connor Holowachuk <connor_chuk@yahoo.com>
2019-05-15 17:51:30 -0400

    Added addExercise and addWorkout

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,56 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
+exports.createExercise = functions.https.onRequest((request, response) => {
+
+  let desc = request.body.description;
+  let timd = request.body.is_timed;
+  let name = request.body.name;
+
+  let exerciseRef = admin
+    .database()
+    .ref('/exercises')
+    .push({
+      description: desc,
+      is_timed: timd,
+      name: name
+    })
+
+  let pushID = exerciseRef.key;
+  response.json({
+    pushKey: pushID
+  })
+});
+
+exports.createWorkout = functions.https.onRequest((request,response) => {
+  let name = request.body.name;
+  let image_A = request.body.image_A;
+  let image_B = request.body.image_B;
+  let target_gender = request.body.target_gender;
+  let estimated_length = request.body.estimated_length;
+  let exercises = request.body.exercises;
+
+  let workoutRef = admin
+    .database()
+    .ref('/workouts')
+    .push({
+      name: name,
+      image_A: image_A,
+      image_B: image_B,
+      target_gender: target_gender,
+      exercises: exercises,
+      estimated_length: estimated_length
+    })
+
+  let pushID = workoutRef.key;
+  response.json({
+    pushKey: pushID
+  })
+});
+
+
+
 //Function 1
 exports.createUID = functions.https.onRequest((request, response) => {
 
